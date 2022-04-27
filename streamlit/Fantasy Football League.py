01cf616f44569a0fbfd58c1b62d7638364057179
Sumedh Khodke <sumedh.khodke@gmail.com>
2022-04-26 23:43:11 -0400

    add streamlit_authentication

--- streamlit/Fantasy Football League.py
+++ streamlit/Fantasy Football League.py
@@ -4,6 +4,17 @@ import numpy as np
 import select_query, custom_query
 from PIL import Image
 import os, sys
+import streamlit as st
+import streamlit_authenticator as stauth
+
+names = ['Sumedh','Omkar','Saiteja','ron']
+usernames = ['sumedh','omkar','saiteja','ron']
+passwords = ['dmql@123','dmql@123','dmql@123','dmql@123']
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+   'some_cookie_name', 'some_signature_key', cookie_expiry_days=0)
 
 try:
     navigate = st.sidebar.radio('Menu',['About The Application','ER diagram','Run Select Query','Run Custom Query'])
@@ -28,17 +39,25 @@ try:
         st.dataframe(df,width = 15000,height = 500)
 
     if navigate == "Run Custom Query":
-        c_query = st.text_input('Enter your custom query :','SELECT * FROM public.player;')
-        df = custom_query.custom_query(c_query)
-        st.write("")
-        if df is None:
-            st.write("You do not have enough permissions to run this query")
-        else:
-            st.dataframe(df,width = 15000,height = 500)
+        name, authentication_status, username = authenticator.login('Login', 'sidebar')
+        if authentication_status:
+            st.write("You are executing database queries as *%s*" % (name))
+            c_query = st.text_input('Enter your custom query :','SELECT * FROM public.player;')
+            df = custom_query.custom_query(c_query,username)
+            if df is None:
+                st.write("You do not have enough permissions to run this query")
+            else:
+                st.write("")
+                st.dataframe(df,width = 15000,height = 500)
+            if st.session_state['authentication_status']:
+                authenticator.logout('Logout', 'sidebar')
+        elif authentication_status == False:
+            st.error("Username/password is incorrect")
+        elif authentication_status == None:
+            st.warning("Please enter your username and password")
 
 except Exception as e:
     print(e)
     print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    st.error("Oops! :(")
     st.error("Looks like something is not right!")
-    st.error("Please contact site admin if the problem persists")
+    st.error("Please contact site admin")
--- streamlit/custom_query.py
+++ streamlit/custom_query.py
@@ -16,7 +16,7 @@ def connect(params_dic):
         sys.exit(1) 
     return conn
 
-def custom_query(query):
+def custom_query(query,username):
     """
     custom query
     """
@@ -25,7 +25,8 @@ def custom_query(query):
         cur = conn.cursor()
         checks = ['update','insert','delete','drop','create','alter','truncate']
         flag1=False
-        if any(x in query.lower() for x in checks):
+        usernames_list = get_admin_list()
+        if any(x in query.lower() for x in checks) and(username not in usernames_list):
             flag1=True
             df=None
         if not flag1:
@@ -41,4 +42,17 @@ def custom_query(query):
         cur.close()
         conn.close()
         raise error
+
+def get_admin_list():
+    conn = connect(param_dic)
+    cursor = conn.cursor()
+    postgreSQL_select_Query = 'SELECT username FROM public."user" where role_type=\'admin\';'
+    cursor.execute(postgreSQL_select_Query)
+    username_records = cursor.fetchall()
+    usernames_list = []
+    for row in username_records:
+        usernames_list.append(row[0])
+    cursor.close()
+    conn.close()
+    return usernames_list
         
